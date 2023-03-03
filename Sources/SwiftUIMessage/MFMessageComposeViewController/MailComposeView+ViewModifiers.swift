a70c6837efd9fff4da685ea8b9c6a006f6901a81
Edon <edon@valdman.works>
2023-03-02 19:25:41 -0500

    Update MailComposeView+ViewModifiers.swift
    Added a modifier to give access to disabling user attachments in the modal.

--- Sources/SwiftUIMessage/MFMessageComposeViewController/MailComposeView+ViewModifiers.swift
+++ Sources/SwiftUIMessage/MFMessageComposeViewController/MailComposeView+ViewModifiers.swift
@@ -8,5 +8,13 @@
 import SwiftUI
 
 extension View where Self == MailComposeView {
+    /// Disables the camera/attachment button in the message composition view.
+    ///
+    /// In iOS 7.0 and later, call this method to disable the camera/attachment button in the message composition view.
+    func disableUserAttachments(_ disable: Bool = true) -> MailComposeView {
+        var newView = self
+        newView.disableUserAttachments = disable
+        return newView
+    }
 }
 
