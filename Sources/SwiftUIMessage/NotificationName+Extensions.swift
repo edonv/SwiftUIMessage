fdf2639e76df86a68132dd634c7309c62a6645d8
Edon <edon@valdman.works>
2023-03-02 22:31:35 -0500

    Renamed MCViewDidFinishResultKey/Notification.Name
    Renamed MCViewDidFinishResultKey to DidFinishResultKey and Notification.Name from .MCViewDidFinish to .MessageComposeViewDidFinish. This is to make them different from the ones for MailComposeView and to be more clear for their own uses.

--- Sources/SwiftUIMessage/NotificationName+Extensions.swift
+++ Sources/SwiftUIMessage/NotificationName+Extensions.swift
@@ -8,8 +8,11 @@
 import Foundation
 
 extension Notification.Name {
+    /// Posted when a ``MessageComposeView`` completes and closes.
+    ///
+    /// Upon receiving this notification, query its `userInfo` dictionary with the ``MessageComposeView/DidFinishResultKey`` key. It will be of [MessageComposeResult](https://developer.apple.com/documentation/messageui/messagecomposeresult) type.
+    public static let MessageComposeViewDidFinish = Notification.Name("SwiftUIMessage.MessageComposeViewDidFinish")
+    
     /// Posted when a ``MailComposeView`` completes and closes.
     ///
-    /// Upon receiving this notification, query its userInfo dictionary with the ``MailComposeView/MCViewDidFinishResultKey`` key. It will be of [MessageComposeResult](https://developer.apple.com/documentation/messageui/messagecomposeresult) type.
-    public static let MCViewDidFinish = Notification.Name("SwiftUIMessage.MCViewDidFinish")
 }
