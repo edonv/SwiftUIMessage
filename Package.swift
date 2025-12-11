98ab4c484816a95d516de8f6b8a29136834664a0
Edon <edon@valdman.works>
2025-12-11 12:01:18 -0600

    Update Package.swift
    Updated indentation.

--- Package.swift
+++ Package.swift
@@ -14,7 +14,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftUIMessage",
-            targets: ["SwiftUIMessage"]),
+            targets: ["SwiftUIMessage"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -30,6 +31,7 @@ let package = Package(
         ),
         .testTarget(
             name: "SwiftUIMessageTests",
-            dependencies: ["SwiftUIMessage"]),
+            dependencies: ["SwiftUIMessage"]
+        ),
     ]
 )
