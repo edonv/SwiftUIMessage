ded5e063adf47202673a64aa37a269b9edd17f2e
Edon <edon@valdman.works>
2025-12-11 12:00:59 -0600

    Update Package.swift
    Updated platforms.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
     name: "SwiftUIMessage",
     platforms: [
         .iOS(.v13),
-        .macCatalyst(.v13)
+        .macCatalyst(.v13),
+        .visionOS(.v1),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
